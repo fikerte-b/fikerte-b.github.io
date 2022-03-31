5a790779037ba63b19abb32f58205c82d5946204
fikerte-b <fikirbenti@gmail.com>
2022-03-30 23:06:36 -0500

    dom assignment

---
+++ CS315WAP/images/assignment3DOM/dom1.js
@@ -0,0 +1,6 @@
+function add(){
+    let textInput = document.getElementById("IdInput");
+    let textOutput = documnet.getElementById("IdOutput");
+    textOutput.value = textInput.value;
+    textInput.value = "";
+}
